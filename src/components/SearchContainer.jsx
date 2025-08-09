3dd2b34a440a099a888c6115f01dc1229e469f02
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-08-09 23:06:59 +0530

    Modified SearchContainer.jsx by making it Responsive for all devices

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -13,24 +13,24 @@ const SearchContainer = () => {
 
     return (
         <div className="search-container my-5 w-full p-5">
-            <div className="search-box w-full min-h-20 border-0 rounded-3xl grid grid-cols-12 p-2">
+            <div className="search-box w-full min-h-20 border-0 rounded-3xl grid gap-2 p-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-12">
                 {/* search-bar */}
-                <div className='col-start-1 col-end-3 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
                     <label htmlFor="search-input" className='font-medium cursor-pointer my-2'>Where</label>
                     <input type="search" name="" className='w-full border-0 outline-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' placeholder='Search destinations' id="search-input" autoComplete='off' />
                 </div>
                 {/* check-in bar */}
-                <div className='col-start-3 col-end-5 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
                     <label htmlFor="checkin-input" className='font-medium cursor-pointer my-2'>Check in</label>
                     <input type="date" name="" className='w-full border-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' placeholder='Search destinations' id="checkin-input" autoComplete='off' />
                 </div>
                 {/* check-out bar */}
-                <div className='col-start-5 col-end-7 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 border-r-0 xl:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
                     <label htmlFor="checkout-input" className='font-medium cursor-pointer my-2'>Check out</label>
                     <input type="date" name="" className='w-full border-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' placeholder='Search destinations' id="checkout-input" autoComplete='off' />
                 </div>
                 {/* add guests bar */}
-                <div className='col-start-7 col-end-9 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
                     <label className='font-medium cursor-pointer my-2' onClick={handleGuestModal}>Who</label>
                     <div className='w-full flex items-center gap-0.5 relative'>
                         <input type="text" name="" className='w-[80%] border-0 outline-0 dark:text-white' placeholder='Add guests' id="guest-input" autoComplete='off' onClick={handleGuestModal} readOnly />
@@ -41,7 +41,7 @@ const SearchContainer = () => {
                     </div>
                 </div>
                 {/* category bar */}
-                <div className='col-start-9 col-end-11 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
                     <label htmlFor='category-input' className='font-medium cursor-pointer my-2'>Category</label>
                     <select
                         name="Headline"
@@ -63,7 +63,7 @@ const SearchContainer = () => {
                         <option value="forts">Forts</option>
                     </select>
                 </div>
-                <div className='col-start-11 col-end-13 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-center justify-center flex-col p-2 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-center justify-center flex-col p-2 transition-all duration-200'>
                     <button type='button' className='w-auto flex items-center justify-between gap-2 rounded-md bg-amber-600 text-white px-3 py-1 active:scale-90 transition-transform duration-200'>Search <FaSearch /></button>
                 </div>
             </div>
