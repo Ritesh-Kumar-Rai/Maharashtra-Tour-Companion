78f94fd74c6dc746b161f6c5d98d01a2222b12d2
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-08-11 21:32:54 +0530

    modified SearchContainer by removing transition-all and duration-200 from input tags which causing some bug with placeholder text like after transition from dark to light mode the placeholder text stays white in color until user interaction. And at lastly removed shadow-sm from select element

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -15,9 +15,9 @@ const SearchContainer = () => {
         <div className="search-container my-5 w-full p-5">
             <div className="search-box w-full min-h-20 border-0 rounded-3xl grid gap-2 p-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-12">
                 {/* search-bar */}
-                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800'>
                     <label htmlFor="search-input" className='font-medium cursor-pointer my-2'>Where</label>
-                    <input type="search" name="" className='w-full border-0 outline-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' placeholder='Search destinations' id="search-input" autoComplete='off' />
+                    <input type="search" name="" className='w-full border-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' placeholder='Search destinations' id="search-input" autoComplete='off' />
                 </div>
                 {/* check-in bar */}
                 <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
@@ -30,10 +30,10 @@ const SearchContainer = () => {
                     <input type="date" name="" className='w-full border-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' placeholder='Search destinations' id="checkout-input" autoComplete='off' />
                 </div>
                 {/* add guests bar */}
-                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800 transition-all duration-200'>
+                <div className='md:col-span-2 lg:col-span-1 xl:col-span-2 col-span-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-3xl hover:border-0 flex items-start justify-center flex-col p-2 lg:border-r-2 border-gray-200 dark:border-gray-800'>
                     <label className='font-medium cursor-pointer my-2' onClick={handleGuestModal}>Who</label>
                     <div className='w-full flex items-center gap-0.5 relative'>
-                        <input type="text" name="" className='w-[80%] border-0 outline-0 dark:text-white' placeholder='Add guests' id="guest-input" autoComplete='off' onClick={handleGuestModal} readOnly />
+                        <input type="text" placeholder='Add guests' className='w-[80%] border-0 focus:outline-slate-500 dark:focus:outline-amber-500 dark:text-white' id="guest-input" autoComplete='off' onClick={handleGuestModal} readOnly />
                         <IoIosClose className={`w-[20%] cursor-pointer bg-slate-300 dark:bg-slate-800 rounded-full transition-all active:scale-70`} size={28} />
 
                         {/* modal here */}
@@ -46,9 +46,9 @@ const SearchContainer = () => {
                     <select
                         name="Headline"
                         id="category-input"
-                        className="mt-0.5 w-full rounded border-gray-200 shadow-sm sm:text-sm md:text-md py-1 dark:border-gray-600 dark:bg-gray-900 dark:text-white"
+                        className="mt-0.5 w-full rounded border-gray-200 sm:text-sm md:text-md py-1 dark:border-gray-600 dark:bg-gray-900 dark:text-white"
                     >
-                        <option value="">Please select place category</option>
+                        <option value="">Select place category</option>
                         <option value="beaches">Beaches</option>
                         <option value="nature&wildlife">Nature & Wildlife</option>
                         <option value="UNESCO-heritage-sites">UNESCO Heritage Sites</option>
